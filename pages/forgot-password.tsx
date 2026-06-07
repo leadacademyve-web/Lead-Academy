9bb816b0ead65a4528dac4f96e022d557af512f8
leadacademyve-web <leadacademyve@gmail.com>
2026-06-07 11:42:14 -0600

    Update forgot-password.tsx

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -34,7 +34,7 @@ export default function ForgotPasswordPage() {
         <div className="auth-premium-form">
           <div className="auth-brand-block">
             <img
-              public="/logo.png"
+              src="public/logo.png"
               alt="Lead Academy"
               className="auth-logo-image"
               style={{
